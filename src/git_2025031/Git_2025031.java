9460e03407f2e5a6f21147e3cff91cce250273bb
zayabatkhuyag <zayab2112@gmail.com>
2025-04-29 22:08:49 +0100

    Homework - Simple Git Test

---
+++ src/git_2025031/Git_2025031.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package git_2025031;
+
+/**
+ *
+ * @author zayab
+ */
+public class Git_2025031 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
