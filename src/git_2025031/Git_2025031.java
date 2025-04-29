3d859c7304f6dece78f7a3ee149fcd3fb7ba1db9
zayabatkhuyag <zayab2112@gmail.com>
2025-04-29 22:22:27 +0100

    Added a simple print

--- src/git_2025031/Git_2025031.java
+++ src/git_2025031/Git_2025031.java
@@ -14,7 +14,8 @@ public class Git_2025031 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        System.out.println("Buyanzaya Batkhuyag - 2025031");
     }
     
 }
